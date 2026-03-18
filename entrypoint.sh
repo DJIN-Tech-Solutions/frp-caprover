55b8699294c3f2cf0be7829f5e62492803d88018
dtechmaster <diniz@djin-tech.com>
2026-03-19 08:24:13 +0900

    Add web server user and password substitutions

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,8 @@
 sed -i "s|#TOKEN#|$TOKEN|g" /frp/server.toml
 sed -i "s|#SUBDOMAIN_HOST#|$SUBDOMAIN_HOST|g" /frp/server.toml
 
+# added by diniz
+sed -i "s|#WEBSERVER_USER#|$WEBSERVER_USER|g" /frp/server.toml
+sed -i "s|#WEBSERVER_PASSWORD#|$WEBSERVER_PASSWORD|g" /frp/server.toml
+
 /frp/frps -c /frp/server.toml
